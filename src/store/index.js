d3216331aabe01fd86211ba8af09c640197287ef
MarcusShen <killthefullmoon@gmail.com>
2023-05-19 00:19:17 -0400

    Updates

--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import demoSlice from './features/demoSlice'
-const store = configureStore({
-  reducer:{
-    demo:demoSlice, //首页显示的五个变量[合约地址，平台运行时间...]
-  }
-})
-export default store
